b3bb940101e74c8a631d8cdfdfb37b1d287c72cc
umeshkumar111083 <umesh.kumar24@alumni.iitd.ac.in>
2025-01-12 03:46:18 +0900

    deleted unused files

--- hospital-food-management/pages/test.tsx
+++
@@ -1,7 +0,0 @@
-export default function TestPage() {
-    return (
-      <div className="flex items-center justify-center min-h-screen bg-blue-500 text-white text-3xl font-bold">
-        Tailwind CSS is Working! 🚀
-      </div>
-    );
-  }
